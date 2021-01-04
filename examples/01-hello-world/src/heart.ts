f5da783a68e80241372322486dac4cf566050623
Tony Pinkevych <tony@untimestudio.com>
2021-01-04 04:32:16 +0200

    docs: remove unnecessary comment

--- examples/01-hello-world/src/heart.ts
+++ examples/01-hello-world/src/heart.ts
@@ -4,7 +4,6 @@ import { common } from './common'
 const { commands, dispatch } = createHeart({
   contexts: { common },
 
-  // механизм сохранения/загрузки ивентов. Т.е. любой адаптер к базе данных
   eventStore: {
     saveEvents: async (events: any[]) => {
       console.log('eventStore.saveEvents', events)
